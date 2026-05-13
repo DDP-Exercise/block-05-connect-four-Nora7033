db50232acfeee1840c6d0ece81671346f1dda273
Nora7033 <s2510892019@fhooe.at>
2026-05-13 19:19:25 +0200

    final?

--- js/view.polished.js
+++ js/view.polished.js
@@ -66,16 +66,3 @@ document.addEventListener("model:gameOverEvent", function(e) {
         statusDisplay.textContent = "Unentschieden. Die nächste Schlacht wird entscheiden.";
     }
 });
-
-//TODO: Think of this view as your game board.
-//      Your view should listen to various custom events of your model.
-//      For each event of your model, there should be a clear visual
-//      representation of what's going on.
-
-//TODO: Update the field. Show the whole battlefield with all the stones
-//      that are already played.
-
-//TODO: Show the current player
-
-//TODO: Notify the player when the game is over. Make it clear how the
-//      Game ended. If it's a win, show the winning stones.
